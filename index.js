5e274181d8a01b11f363d4afa4af6d8f2b6d142b
rishabkhivsara <rishabjain3500@gmail.com>
2023-03-12 02:41:57 +0530

    refactor(CAROUSEL): Added comments and refactored logic.
    more maintainable and reusable.

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 const slides = document.querySelectorAll(".main-frame");
 const prevBtn = document.querySelector(".left");
 const nextBtn = document.querySelector(".right");
-const a1Dots = document.querySelector(".a1-dots");
+const dotsContainer = document.querySelector(".a1-dots");
 
 let curSlide = 0;
 const slideLength = slides.length - 1;
 
-// if (curSlide > slideLength) curSlide = 0;
-// if (curSlide < 0) curSlide = slideLength;
-
 for (const [idx, slide] of slides.entries()) {
+  // Dynamically generate the swiper dots based on the no. of slides.
   let dot = document.createElement("div");
   dot.classList.add("dot");
-  dot.dataset.dot = `${idx}`;
-  dot.addEventListener("click", (e) => {
-    console.log(`Current Slide Index: ${e.target.dataset.dot}`);
-    let newSlide = parseInt(e.target.dataset.dot);
-    changeSlideDot(newSlide, curSlide);
-  });
-  a1Dots.appendChild(dot);
 
+  // Add a dataset attribute to the dynamically generated divs based on their index.
+  dot.dataset.dot = `${idx}`;
+  // Append the newly generated dots to the parent.
+  dotsContainer.appendChild(dot);
+
+  // If the current Index is equal to the slide Index, make it active.
   if (idx === curSlide) {
     slide.className = "main-frame";
     dot.classList.add("dot-active");
@@ -27,13 +24,26 @@ for (const [idx, slide] of slides.entries()) {
     slide.className = "main-frame--zero";
   }
 
+  // Translate every slide based on its index and set their inital position.
+  // Show the first slide and hide the rest
   slide.style.transform = `translateX(${idx * 100}%)`;
   showArrow();
 }
+
+// Attach eventListener to the dotsContainer and update the currentSlide Index ${curSlide} based on its custom dataset value. Change the slide based on this new Index. Event Delegation ✨✨✨
+dotsContainer.addEventListener("click", (event) => {
+  if (event.target.classList.contains("dot")) {
+    const dotIndex = parseInt(event.target.dataset.dot);
+    goToSlide(dotIndex, curSlide);
+  }
+});
+
+// Create a global scoped variable in memory of all dots to prevent fetching them everytime.
 let dot = document.querySelectorAll(".dot");
 dot = [...dot];
 
-const changeSlideDot = (newSlide, currSlide) => {
+
+const goToSlide = (newSlide, currSlide) => {
   if (newSlide === currSlide) return;
 
   if (newSlide > currSlide) {
@@ -48,53 +58,15 @@ const changeSlideDot = (newSlide, currSlide) => {
 };
 
 const runPrev = () => {
-  // let dot = document.querySelectorAll(".dot");
-  // dot = [...dot];
   curSlide--;
-  // if (curSlide > slideLength) curSlide = 0;
-  // if (curSlide < 0) curSlide = slideLength;
-
-  console.log(`Current Slide Index: ${curSlide}`);
-
   showArrow();
-
-  //   move slide by -100%
-  slides.forEach((slide, idx) => {
-    if (idx === curSlide) {
-      slide.className = "main-frame";
-      dot[idx].classList.add("dot-active");
-    } else {
-      slide.className = "main-frame--zero";
-      dot[idx].classList.remove("dot-active");
-    }
-
-    slide.style.transform = `translateX(${100 * (idx - curSlide)}%)`;
-  });
+  applyStylesBasedOnIndex();
 };
 
 const runNext = () => {
   curSlide++;
-  // let dot = document.querySelectorAll(".dot");
-  // dot = [...dot];
-  // if (curSlide > slideLength) curSlide = 0;
-  // if (curSlide < 0) curSlide = slideLength;
-
-  console.log(`Current Slide Index: ${curSlide}`);
-
   showArrow();
-
-  //   move slide by -100%
-  slides.forEach((slide, idx) => {
-    if (idx === curSlide) {
-      slide.className = "main-frame";
-      dot[idx].classList.add("dot-active");
-    } else {
-      slide.className = "main-frame--zero";
-      dot[idx].classList.remove("dot-active");
-    }
-
-    slide.style.transform = `translateX(${100 * (idx - curSlide)}%)`;
-  });
+  applyStylesBasedOnIndex();
 };
 
 function showArrow() {
@@ -104,6 +76,23 @@ function showArrow() {
     nextBtn.style.visibility = "visible";
     prevBtn.style.visibility = "visible";
   }
+}
+
+function applyStylesBasedOnIndex() {
+
+
+  slides.forEach((slide, idx) => {
+    if (idx === curSlide) {
+      slide.className = "main-frame";
+      dot[idx].classList.add("dot-active");
+    } else {
+      slide.className = "main-frame--zero";
+      dot[idx].classList.remove("dot-active");
+    }
+
+    //   move slide by -100%
+    slide.style.transform = `translateX(${100 * (idx - curSlide)}%)`;
+  });
 }
 
 prevBtn.addEventListener("click", runPrev);
